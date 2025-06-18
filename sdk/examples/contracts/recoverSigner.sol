619d44752cbdc2016f4ed905331e79d3dc40a0ab
Lucas Rack <lucas.rack@lambdaclass.com>
2025-06-18 18:03:04 -0300

    Deploy contract

--- sdk/examples/contracts/recoverSigner.sol
+++ sdk/examples/contracts/recoverSigner.sol
@@ -1,25 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
 contract RecoverSigner {
-    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
     using Address for address;
-    
+
     event RecoveredSigner(
         address signer
     );
-    
-    function recoverSigner (
+
+    function recoverSigner(
         bytes32 message,
         bytes memory signature
     ) public {
         bytes32 hash = message.toEthSignedMessageHash();
-        address signer = hash.recover(signature);
-        emit RecoveredSigner(
-            signer
-        );
+        address signer = ECDSA.recover(hash, signature);
+        emit RecoveredSigner(signer);
     }
 }
+
+// generate binary:
+// npm init -y
+// npm install @openzeppelin/contracts
+// solc --bin recoverSigner.sol --base-path . --include-path node_modules/
--- sdk/examples/deploy_contract_with_keystore.rs
+++ sdk/examples/deploy_contract_with_keystore.rs
@@ -1,9 +1,13 @@
 use ethrex_common::{H160, H256};
 use rex_sdk::client::EthClient;
+use rex_sdk::client::eth::get_address_from_secret_key;
 use rex_sdk::{
     keystore::{create_new_keystore, load_keystore_from_path},
     sign::sign_hash,
 };
+
+const RPC_URL: &str = "https://ethereum-holesky-rpc.publicnode.com";
+
 //Habría que codear un contrato de ejemplo en solidity
 // con una función que recupere al signer dado un mensaje y una firma y lo emita en un log
 
@@ -16,10 +20,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let private_key = load_keystore_from_path(None, "ContractKeystore", "LambdaClass")?;
 
     // Connect the client to a node
-    let client = EthClient::new("127.0.0.1:8545");
+    let client = EthClient::new(RPC_URL);
+
+    // Get address from private key
+    let address = get_address_from_secret_key(&private_key)?;
 
     // Deploy a contract.
-    // client.deploy(deployer, deployer_private_key, init_code, overrides);
+    client.deploy(address, private_key, init_code, overrides);
 
     // Get the current block (for later).
     let from_block = client.get_block_number().await?;
