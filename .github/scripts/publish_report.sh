b628676c9579f0d33d48113c000bf3ee0cecb84e
Lucas Rack <lucas.rack@lambdaclass.com>
2025-07-22 12:10:00 -0300

    Init

---
+++ .github/scripts/publish_report.sh
@@ -0,0 +1,22 @@
+curl -X POST $1 \
+-H 'Content-Type: application/json; charset=utf-8' \
+--data @- <<EOF
+$(jq -n --arg text "$2" '{
+    "blocks": [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": "Daily Rex report"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": $text
+            }
+        }
+    ]
+}')
+EOF
