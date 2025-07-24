c724a24a7eb13d928d3aa7be5c2d020db0f388f5
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-07-24 17:59:52 -0300

    refactor: remove tracing dep and use inspect err

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,12 +2,8 @@ use rex::cli;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::ERROR)
-        .init();
-
-    if let Err(err) = cli::start().await {
+    let _ = cli::start().await.inspect_err(|err| {
         println!("\x1b[31;1mError:\x1b[0m execution failed: {err:?}");
         std::process::exit(1);
-    }
+    });
 }
