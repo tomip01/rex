02ca8a3f748e4c29d4264e064fa07c920c86fd48
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2025-07-25 09:26:19 -0300

    refactor: use erprintln

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,7 +3,7 @@ use rex::cli;
 #[tokio::main]
 async fn main() {
     let _ = cli::start().await.inspect_err(|err| {
-        println!("\x1b[31;1mError:\x1b[0m execution failed: {err:?}");
+        eprintln!("\x1b[31;1mError:\x1b[0m execution failed: {err:?}");
         std::process::exit(1);
     });
 }
